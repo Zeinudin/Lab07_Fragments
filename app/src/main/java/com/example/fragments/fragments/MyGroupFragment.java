618717fda0043a99afadd38664c9519c22cf815b
Zeinudin <Lirov_z@iuca.kg>
2021-03-16 17:28:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fragments/fragments/MyGroupFragment.java
@@ -0,0 +1,33 @@
+package com.example.fragments.fragments;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.fragments.R;
+
+
+public class MyGroupFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_my_group, container, false);
+
+        ImageView imageview = (ImageView) view.findViewById(R.id.fotka);
+        Drawable drawable = ContextCompat.getDrawable(view.getContext(), R.drawable.gggggg);
+        imageview.setImageDrawable(drawable);
+
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/fragments/fragments/MyPhotoFragment.java
@@ -0,0 +1,20 @@
+package com.example.fragments.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fragments.R;
+
+public class MyPhotoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_photo, container, false);
+    }
+}
